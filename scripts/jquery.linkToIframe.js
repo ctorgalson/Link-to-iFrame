87cf266b5aaa7a79ed6195961ae94a2fc52dc17a
Christopher Torgalson <chris@raisedeyebrow.com>
2012-01-11 11:39:31 -0800

    Slightly rewritten version of old plugin; first commit to git repo

---
+++ scripts/jquery.linkToIframe.js
@@ -0,0 +1,56 @@
+/**
+ * jQuery link to iFrame Plugin
+ *
+ * A small jQuery plugin to transform a hyperlink into an iframe with src
+ * attribute equivalent to the original link's href attribute.
+ *
+ * @version 1.0
+ * @author Christopher Torgalson <manager@bedlamhotel.com>
+ * @param object overrides Configuration options:
+ *
+ *   --  height: height, in pixels, of iframe (units not required!)
+ *   --  iframeHeading: Header, if any for the iframe
+ *   --  iframeContainer: Container for iframe and heading
+ *   --  width: width, in pixels, of iframe (units not required!)
+ *
+ * @example
+ *
+ * // Replaces the link with the id attribute 'iframe' with and iframe showing
+ * // http://www.example.com:
+ * $('#iframe').linkToIframe();
+ */
+(function($){
+  $.fn.linkToIframe = function(overrides) {
+    // Set default values:
+    var defaults = {
+      height: 300,
+      iframeHeading: $('<h2>iFrame</h2>'),
+      iframeParent: $('<div/>'),
+      width: 400
+    },
+    settings = $.extend({}, defaults, overrides);
+    
+    // Work on the collection:
+    return this.each(function(){
+      // Do nothing if this is not a link:
+      if (this.tagName.toLowerCase() == 'a') {
+        // Default vars; build iframe:
+        var $current = $(this), 
+            iframe = $('<iframe/>')
+              .attr({
+                src: $current.attr('href'),
+                height: settings.height,
+                width: settings.width
+              })
+              .appendTo(settings.iframeParent.append(settings.iframeHeading));
+        // Replace the incoming link with the new iframe:
+        $current.replaceWith(settings.iframeParent);
+      }
+    });
+  };
+  
+  Drupal.behaviors.linkToIframe = function(context){
+    $('#iframetest').linkToIframe();
+  }
+})(jQuery);
+/* jquery.linkToIframe.js */
